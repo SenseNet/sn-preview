a239c070ef41c96f4a5ea3e7a8d9bbda6b9ee835
tusmester <tusmester@gmail.com>
2019-02-25 15:59:56 +0100

    Netstandard/preview controller (#11)
    * Update sensenet references to their latest versions.
    * Convert Controller project to .Net Standard.

--- src/Preview/Preview.Controller/Properties/AssemblyInfo.cs
+++ src/Preview/Preview.Controller/Properties/AssemblyInfo.cs
@@ -2,24 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-#if DEBUG
-[assembly: AssemblyTitle("SenseNet.Preview.Controller (Debug)")]
-#else
-[assembly: AssemblyTitle("SenseNet.Preview.Controller (Release)")]
-#endif
-
-[assembly: AssemblyDescription("Preview backend operations for the sensenet ECM platform.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Sense/Net Inc.")]
-[assembly: AssemblyCopyright("Copyright © Sense/Net Inc.")]
-[assembly: AssemblyProduct("sensenet ECM")]
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("64ffbb21-44db-4354-9379-757aebe2fa74")]
-
-[assembly: AssemblyVersion("7.1.1.0")]
-[assembly: AssemblyFileVersion("7.1.1.0")]
-
-[assembly: AssemblyInformationalVersion("7.1.1")]
