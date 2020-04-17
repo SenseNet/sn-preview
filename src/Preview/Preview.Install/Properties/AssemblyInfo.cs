114013d03d2e69994423d0777ff15ed2b0c97421
tusmester <tusmester@gmail.com>
2020-04-17 19:28:01 +0200

    Add separate Instal project. (#26)

---
+++ src/Preview/Preview.Install/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: InternalsVisibleTo("SenseNet.Preview.Tests")]
+
+[assembly: AssemblyTrademark("Sense/Net Inc.")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("64ffbb21-44db-4354-9379-757aebe2fa74")]
