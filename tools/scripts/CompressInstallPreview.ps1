be1671e68c1945bff6573e015b9fa7530c8ba3ec
tusmester <tusmester@gmail.com>
2020-05-05 15:45:33 +0200

    Build preview install package into the Install library as an embedded resource.

---
+++ tools/scripts/CompressInstallPreview.ps1
@@ -0,0 +1,11 @@
+﻿$srcPath = [System.IO.Path]::GetFullPath(($PSScriptRoot + '\..\..\src'))
+$installFolderPreview = "$srcPath\Preview\nuget\content\Admin\tools"
+$installPackagePathPreview = "$installFolderPreview\install-preview.zip"
+
+New-Item $installFolderPreview -Force -ItemType Directory
+
+# Create the install package.
+Compress-Archive -Path "$srcPath\Preview\nuget\snadmin\install-preview\*" -Force -CompressionLevel Optimal -DestinationPath $installPackagePathPreview
+
+# Copy the install package to the project directory
+Move-Item $installPackagePathPreview -Destination "$srcPath\Preview\Preview.Install" -Force
