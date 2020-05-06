f31f6174b772d89ba61d0e6590a3ca66874e64a2
tusmester <tusmester@gmail.com>
2020-05-06 12:41:53 +0200

    Add extension method to set the dopreview provider.

---
+++ src/Preview/Preview.Controller/PreviewExtensions.cs
@@ -0,0 +1,17 @@
+﻿using SenseNet.Configuration;
+using SenseNet.Preview;
+using SenseNet.Tools;
+
+namespace Preview.Controller
+{
+    public static class PreviewExtensions
+    {
+        public static IRepositoryBuilder UseDocumentPreviewProvider(this IRepositoryBuilder repositoryBuilder, 
+            DocumentPreviewProvider previewProvider)
+        {
+            Providers.Instance.PreviewProvider = previewProvider;
+
+            return repositoryBuilder;
+        }
+    }
+}
