f9a6a7cdcac9edbce425d52714d3a0fe63ed6ed4
tusmester <tusmester@gmail.com>
2022-09-08 14:45:17 +0200

    Add logger to base generator class.

--- src/Preview/Preview/PreviewImageGenerator.cs
+++ src/Preview/Preview/PreviewImageGenerator.cs
@@ -1,22 +1,31 @@
 ﻿using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace SenseNet.Preview
 {
     public abstract class PreviewImageGenerator : IPreviewImageGenerator
     {
+        private readonly ILogger<PreviewImageGenerator> _logger;
+
+        protected PreviewImageGenerator(ILogger<PreviewImageGenerator> logger)
+        {
+            _logger = logger;
+        }
+
         public abstract string[] KnownExtensions { get; }
         public abstract Task GeneratePreviewAsync(Stream docStream, IPreviewGenerationContext context, 
             CancellationToken cancellationToken);
+
         public virtual string GetTaskNameByExtension(string extension)
         {
-            // means default
+            // fallback to default
             return null;
         }
         public virtual string GetTaskTitleByExtension(string extension)
         {
-            // means default
+            // fallback to default
             return null;
         }
         public virtual string[] GetSupportedTaskNames()
