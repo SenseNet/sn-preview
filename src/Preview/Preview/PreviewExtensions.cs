75f2762b8d95d98d80d09bf84b63d030861b7753
tusmester <tusmester@gmail.com>
2022-09-09 17:13:43 +0200

    Preview generators support dependency injection (#36)
    * Move static preview generator methods to an instance manager class.
    
    * Fix extensions class namespace.
    
    * Add logger to base generator class.

---
+++ src/Preview/Preview/PreviewExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SenseNet.Preview;
+
+// ReSharper disable once CheckNamespace
+namespace SenseNet.Extensions.DependencyInjection
+{
+    public static class PreviewExtensions
+    {
+        /// <summary>
+        /// Adds the provided preview provider to the service collection.
+        /// </summary>
+        public static IServiceCollection AddSenseNetPreviewGenerator<T>(this IServiceCollection services) where T : class, IPreviewImageGenerator
+        {
+            return services.AddSingleton<IPreviewImageGenerator, T>();
+        }
+
+        /// <summary>
+        /// Adds the core preview feature to the service collection.
+        /// </summary>
+        public static IServiceCollection AddSenseNetPreview(this IServiceCollection services)
+        {
+            return services.AddSingleton<IPreviewGeneratorManager, PreviewGeneratorManager>();
+        }
+    }
+}
